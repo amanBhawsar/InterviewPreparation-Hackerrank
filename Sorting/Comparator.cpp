8d28396c3643773353faaafbaeb949bf7f195c79
Aman Bhawsar <amanbhawsar99@gmail.com>
2020-06-25 12:18:34 +0530

    added counting inversions and comparator

--- Sorting/Comparator.cpp
+++ Sorting/Comparator.cpp
@@ -1,19 +1,15 @@
-class Checker{
-public:
-    static int comparator(Player a, Player b)  {
-        if(a.score > b.score){
+static int comparator(Player a, Player b)  {
+    if(a.score==b.score)
+        if(a.name < b.name){
             return 1;
-        }
-        else if(a.score < b.score){
+        }else if(a.name > b.name){
             return -1;
+        }else{
+            return 0;
         }
-        else{
-            if(a.name.compare(b.name)<0){
-                return 1;
-            }
-            else{
-                return -1;
-            }
-        }
+    if(a.score > b.score){
+        return 1;
+    }else{
+        return -1;
     }
-};
+}
